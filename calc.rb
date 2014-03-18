6ed65ec31c13cff30d74f57da7a3cfcf7bd52db2
A. Gordon <agordon@wi.mit.edu>
2014-03-18 17:34:00 -0400

    calc: upgrade to version 1.0.2

--- calc.rb
+++ calc.rb
@@ -3,8 +3,8 @@ require "formula"
 class Calc < Formula
   homepage "http://agordon.github.io/calc/"
   head "https://github.com/agordon/calc.git"
-  url "https://s3.amazonaws.com/agordon/calc/src/calc-1.0.0.tar.gz"
-  sha1 "507ffbd88d6d8edce1bab389b9a74eee3b7cf930"
+  url "https://github.com/agordon/calc/releases/download/v1.0.2/calc-1.0.2.tar.gz"
+  sha1 "e58728e55eed78e55ee24d2004e07612987fe1bc"
 
   if build.head?
     depends_on :autoconf => :build
