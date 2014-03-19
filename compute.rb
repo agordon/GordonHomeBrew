252e86d5c37b66e0724411a13eb80c1c3aae28d6
A. Gordon <agordon@wi.mit.edu>
2014-03-19 09:08:00 -0400

    compute: rename from 'calc'

--- compute.rb
+++ compute.rb
@@ -1,10 +1,10 @@
 require "formula"
 
-class Calc < Formula
-  homepage "http://agordon.github.io/calc/"
-  head "https://github.com/agordon/calc.git"
-  url "https://github.com/agordon/calc/releases/download/v1.0.2/calc-1.0.2.tar.gz"
-  sha1 "e58728e55eed78e55ee24d2004e07612987fe1bc"
+class Compute < Formula
+  homepage "http://agordon.github.io/compute/"
+  head "https://github.com/agordon/compute.git"
+  url "https://github.com/agordon/compute/releases/download/v1.0.3/compute-1.0.3.tar.gz"
+  sha1 "ebf33934f18dd5afb48363e16cd44410f8d629b3"
 
   if build.head?
     depends_on :autoconf => :build
