58efe1bedb78cee2ab35283636f70572f7daed47
A. Gordon <agordon@wi.mit.edu>
2014-04-11 19:53:28 -0400

    compute: upgrade to version 1.0.4

--- compute.rb
+++ compute.rb
@@ -3,8 +3,8 @@ require "formula"
 class Compute < Formula
   homepage "http://agordon.github.io/compute/"
   head "https://github.com/agordon/compute.git"
-  url "https://github.com/agordon/compute/releases/download/v1.0.3/compute-1.0.3.tar.gz"
-  sha1 "701096fd061536a621b2b02cbad91662c1a12d78"
+  url "https://s3.amazonaws.com/agordon/compute/src/compute-1.0.4.tar.gz"
+  sha1 "058c36814d071f8334e74c5d01edab94941e8368"
 
   if build.head?
     depends_on :autoconf => :build
